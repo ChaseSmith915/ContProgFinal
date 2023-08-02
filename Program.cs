8c3a8f15c4476a9a6eb6d0cafe7ebe9a150af620
Chase Smith <chaseosmith915@gmail.com>
2023-08-02 16:33:18 -0400

    added swagger

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
@@ -9,12 +9,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseSwaggerUi3();
+app.UseReDoc();
 
 app.UseRouting();
 
